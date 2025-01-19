87282b1c65d1c31c53d4bd88a671f11af618e44b
YoRu <tech.niko.coding@gmail.com>
2025-01-19 12:41:38 +0500

    Version 0.0.3 - GameLoop ready

--- main.cpp
+++ main.cpp
@@ -8,5 +8,17 @@ using namespace std;
 int main()
 {
     // Initialization
+    cout << "Starting the game.." << endl;
+    InitWindow(1600, 900, "Snake Game");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
