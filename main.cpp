d5a19649427049ba0b7e066fbfde3a842399cc63
YoRu <tech.niko.coding@gmail.com>
2025-01-19 13:02:16 +0500

    Version 0.0.4 - FoodClass

--- main.cpp
+++ main.cpp
@@ -5,20 +5,41 @@
 
 using namespace std;
 
+Color bgColor = {58, 90, 64, 255};
+Color dark = {52, 78, 65, 255};
+Color light = {163, 177, 138, 255};
+
+int cellSize = 25;
+int cellCountHor = 64;
+int cellCountVer = 36;
+
+class Food
+{
+public:
+	Vector2 position = {5, 6};
+	void Draw()
+	{
+		DrawRectangle(position.x * cellSize, position.y * cellSize, cellSize, cellSize, light);
+	}
+};
+
 int main()
 {
-    // Initialization
-    cout << "Starting the game.." << endl;
-    InitWindow(1600, 900, "Snake Game");
-    SetTargetFPS(60);
+	// Initialization
+	cout << "Starting the game.." << endl;
+	InitWindow(cellSize * cellCountHor, cellSize * cellCountVer, "Snake Game");
+	SetTargetFPS(60);
 
-    while (!WindowShouldClose())
-    {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        EndDrawing();
-    }
+	Food food = Food();
 
-    CloseWindow();
-    return 0;
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground(bgColor);
+		food.Draw();
+		EndDrawing();
+	}
+
+	CloseWindow();
+	return 0;
 }
